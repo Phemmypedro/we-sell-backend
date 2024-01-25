2fea5f27713574cf3079a27f7cb34be6172ff14a
core.pager=cat <core.pager=cat@email.com>
2024-01-25 03:05:10 -0800

    inital commit

---
+++ products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://cdn.alloallo.media/catalog/product/apple/iphone/iphone-12-pro-max/iphone-12-pro-max-gold.jpg",
+  },
+  {
+    id: 2,
+    name: "PHEMMY PEDRO ",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://www.pedroshoes.com/dw/image/v2/BCWJ_PRD/on/demandware.static/-/Sites-pd-products/default/dw9b24c6bb/images/hi-res/2024-L2-PM1-76210245-1-41-5.jpg?sw=1152&sh=1536",
+  },
+  {
+    id: 3,
+    name: "PHEMMY PEDRO",
+    brand: "Shirt",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://static1.squarespace.com/static/644a4a21fc3a6b04971f46be/644a874a0f6a5837d367013e/648731cc4a9a1b6f8637d105/1686581717351/Flat+Lay.jpg?format=1500w",
+  },
+];
+
+module.exports = products;
---
+++ utils/cloudinary.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+const cloudinaryModule = require("cloudinary");
+
+dotenv.config();
+const cloudinary = cloudinaryModule.v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+cloudinary.api.create_upload_preset({
+  name: "online-shop",
+  folder: "products",
+  allowed_formats: "jpg, png, jpeg",
+});
+
+module.exports = cloudinary;
